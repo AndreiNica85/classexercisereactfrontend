fffe8b089e43b280a03f46346899582dfa5415c7
Rodrigo Gamero <rodrigo.gamero.luque@gmail.com>
2022-10-31 12:11:57 +0100

    Project initialization

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home/Home"
+
+
+function App() {
+  return (
+
+      <Routes>
+          <Route index element={<Home />} />
+          {/* <Route path="/flights" element={<Flights />} /> */}
+      </Routes>
+
+  );
+}
+
+export default App;
