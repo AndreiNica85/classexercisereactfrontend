ccf638ae064d94a2a72db36101379e64c826d914
Rodrigo Gamero <rodrigo.gamero.luque@gmail.com>
2022-11-02 16:16:27 +0100

    Changing SearchBar

--- src/components/Flights/Flights.js
+++ src/components/Flights/Flights.js
@@ -1,16 +1,29 @@
 import React from "react";
-import SearchBar from "../SearchBar/SearchBar";
 import "./../SearchBar/SearchBar.css"
 import NavBar from "../Home/NavBar";
 import "./../Home/navBar.css"
+import axios from "axios";
 import "./../Home/Home.css";
+import { useState } from "react";
+import SearchBar from "../SearchBar/SearchBar";
+
+
 
 function Flights(){
+
+    const [origin, setOrigin] = useState();
+    const originHandler = (e) => {
+        setOrigin(e.target.value);
+        return origin;
+    }
+
+    const origins = ['Seville', 'Madrid', 'Barcelona']
 
     return (
     <div>
         <NavBar />
-        <SearchBar />
+        <SearchBar origin={origins}/>
+        
     </div>
     );
 
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,80 +1,9 @@
 import React from "react";
-import axios from "axios";
-import { useState, useEffect } from "react";
-import "./SearchBar.css";
-import FlightCard from "../FlighCard/FlightCard";
-import Options from "./Options";
 
-function SearchBar() {
-
-  const [origin, setOrigin] = useState();
-
-  const originHandler = (e) => {
-    e.preventDefault();
-    setOrigin(document.querySelector(".destination").value);
-    console.log(GetDestinations());
-  };
+class SearchBar extends React.Component {
 
 
 
-  function GetDestinations(){
-
-    let urlflights = "http://localhost:8080/flights/cities/from/" + origin;
-
-    const [flights, setPosts] = useState([]);
-
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          const res = await axios.get(urlflights);
-          setPosts(res.data);
-        } catch (err) {
-          console.log(err);
-        }
-      }
-      fetchData();
-    }, []);
-    return <div>{flights}</div>
-
-  }
-
-
-  let url = "http://localhost:8080/flights";
-
-  const ShowPosts = () => {
-    const [posts, setPosts] = useState([]);
-
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          const res = await axios.get(url);
-          setPosts(res.data);
-        } catch (err) {
-          console.log(err);
-        }
-      }
-      fetchData();
-    }, []);
-    return <div>{posts}</div>;
-  };
-
-  const data = ShowPosts();
-
-  return (
-    <div>
-      <div className="selectionDestination">
-        <form>
-          <select className="destination">
-            {data.props.children.map((o) => {
-              return <Options props={o} />;
-            })}
-          </select>
-          <input className="sendInfo" value="Book a flight!" type="submit" onClick={originHandler}></input>
-        </form> 
-      </div> 
-
-    </div>
-  );
 }
 
-export default SearchBar;
+  export default SearchBar;
