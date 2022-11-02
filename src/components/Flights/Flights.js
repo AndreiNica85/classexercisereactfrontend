50b9545919817dfca9e848a8fd3f53e4d9d1bdca
RodrigoGamero <Rodrigo.gamero.luque@gmail.com>
2022-11-02 18:14:19 +0100

    Bug fix

--- src/components/Flights/Flights.js
+++ src/components/Flights/Flights.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import "./../SearchBar/SearchBar.css";
 import NavBar from "../Home/NavBar";
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,66 +1,76 @@
-import React from "react";
+import React, {useState} from "react";
+import Options from "./Options";
+import { useEffect} from "react";
+import axios from "axios";
 
 function SearchBar() {
+
   const [origin, setOrigin] = useState();
 
-
-
-    const [flights, setPosts] = useState([]);
-
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          const res = await axios.get(urlflights);
-          setPosts(res.data);
-        } catch (err) {
-          console.log(err);
-        }
-      }
-      fetchData();
-    }, []);
-    return <div>{flights}</div>;
+  const originHandler = (e) =>{
+   
+    setOrigin(document.querySelector(".destination").value);
+    console.log(origin)
+    e.preventDefault();
   }
 
-  let url = "http://localhost:8080/flights";
+  let urlflights = "http://localhost:8080/flights/cities/from/" + origin;
 
-  const ShowPosts = () => {
-    const [posts, setPosts] = useState([]);
+  const ShowFlights = () => {
+    const [flights, setPosts] = useState([]);
 
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          const res = await axios.get(url);
-          setPosts(res.data);
-        } catch (err) {
-          console.log(err);
-        }
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await axios.get(urlflights);
+        setPosts(res.data);
+      } catch (err) {
+        console.log(err);
       }
-      fetchData();
-    }, []);
-    return <div>{posts}</div>;
-  };
+    }
+    fetchData();
+  }, []);
+  return <div>{flights}</div>;
+  }
+  
 
-  const data = ShowPosts();
 
-  return (
-    <div>
-      <div className="selectionDestination">
-        <form>
-          <select className="destination">
-            {data.props.children.map((o) => {
-              return <Options props={o} />;
-            })}
-          </select>
-          <input
-            className="sendInfo"
-            value="Book a flight!"
-            type="submit"
-            onClick={originHandler}
-          ></input>
-        </form>
-      </div>
+let url = "http://localhost:8080/flights";
+
+const ShowPosts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await axios.get(url);
+        setPosts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
+  return <div>{posts}</div>;
+};
+
+const data = ShowPosts();
+
+return (
+  <div>
+    <div className="selectionDestination">
+      <form>
+        <select className="destination">
+          {data.props.children.map((o) => {
+            return <Options props={o} />;
+          })}
+        </select>
+        <input className="sendInfo" value="Book a flight!" type="submit" onClick={originHandler}
+        ></input>
+      </form>
     </div>
-  );
+  </div>
+);
 }
 
 export default SearchBar;
