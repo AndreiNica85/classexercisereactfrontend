089c078da04476a1fa0e8f8e6e4a640d3b316574
Rodrigo Gamero <rodrigo.gamero.luque@gmail.com>
2022-11-04 15:53:46 +0100

    qwe

--- src/components/AdministrationPanel/AdministrationPanel.js
+++ src/components/AdministrationPanel/AdministrationPanel.js
@@ -13,11 +13,11 @@ export default function AdministrationPanel(){
         <div className="AdminContainer">
             <div className="AdminElement">
                 <h2>Analytics</h2>
-                <a href="/analytics"><img className="Adminimg" src="https://cdn-icons-png.flaticon.com/512/921/921540.png"></img>
+                <a href="/analytics"><img className="Adminimg" src="https://cdn-icons-png.flaticon.com/512/921/921591.png"></img>
                 </a> </div>
             <div className="AdminElement">
                 <h2>Create New Flight</h2>
-                <a href="/newflight"><img className="Adminimg" src="https://cdn-icons-png.flaticon.com/512/4284/4284017.png"></img>
+                <a href="/newflight"><img className="Adminimg" src="https://cdn-icons-png.flaticon.com/512/723/723276.png"></img>
                 </a>           </div>
         </div>
     </div>
--- src/components/Done/Done.js
+++ src/components/Done/Done.js
@@ -13,7 +13,6 @@ export default function Done(){
     return(<div className="doneContainer">
         <img className="doneImage" alt="doneimage" src="https://cdn-icons-png.flaticon.com/512/1044/1044122.png"></img>
         <h1>Thanks for flying with us</h1>
-        <h1>Have a nice Flight!</h1>
     </div>)
 
 }
